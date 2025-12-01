97fe5fd3f14c735da15ec4bf3e1e8eddd3753ff6
GohTG <gohtg-wm22@student.tarc.edu.my>
2025-12-01 11:05:12 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "employee1.cdfijz89b0ix.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "admin123"
-customdb = "employee1"
+customdb = "employee"
 custombucket = "gohtooange-employee"
 customregion = "us-east-1"
 
